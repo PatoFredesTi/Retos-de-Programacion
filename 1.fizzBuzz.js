d10cbc04fcb905e3c407a2768da2fc7e0884b857
Patricio Fredes <59983502+PatoFredesTi@users.noreply.github.com>
2022-08-31 12:43:29 -0400

    Update 1.fizzBuzz.js

--- 1.fizzBuzz.js
+++ 1.fizzBuzz.js
@@ -1,4 +1,5 @@
-//Ejercicio de Seleccion Basico.
+//Crea un bucle 'for' que imprime los números de 1 al 100. Para los múltiplos de 3 imprime la palabra "Fizz" en vez del número 
+//y para los múltiplos de 5 imprime "Buzz". Para los números que son múltiplos de 3 y de 5, imprime “FizzBuzz”.
 
 for (let i = 1; i < 100; i++){
     if (i % 3 === 0 && i % 5 === 0){
